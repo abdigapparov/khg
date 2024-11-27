31f0db378d3000471c62f7a6fa163a089f5b965c
abdigapparov <abdigapparovberik4@gmail.com>
2024-11-27 16:54:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const name =prompt('ismingiz nima');
+const age =prompt('familiya');
+const score =prompt('yosh');
+
+
+document.querySelector('#name').textContent = name;
+document.querySelector('#age').textContent = age;
+document.querySelector('#score').textContent = score;
+
+const resultElement = document.querySelector('#result');
+
+
+
+
+
